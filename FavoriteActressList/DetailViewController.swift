1ad4f5da88a0cc7622e48d3259546ec623400859
PPTaa <chobo05@naver.com>
2021-02-17 23:27:11 +0900

    add animation effect

--- FavoriteActressList/DetailViewController.swift
+++ FavoriteActressList/DetailViewController.swift
@@ -28,6 +28,49 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        prepareAnimation()
+    }
+//    뷰가 나타나는 시점
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showAnimation()
+        
+    }
+    
+    private func prepareAnimation(){
+//        nameLabelCenterX.constant = view.bounds.width
+//        explainLabelY.constant = view.bounds.width
+        
+        // 뷰속성을 이용한 애니메이션 사전 준비
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        nameLabel.alpha = 0
+        
+        explainLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        explainLabel.alpha = 0
+    }
+    
+    private func showAnimation(){
+//        nameLabelCenterX.constant = 0
+//        explainLabelY.constant = 40
+
+//        // 위치변화 에니메이션(튕기는 효과 추가)
+//        UIView.animate(withDuration: 0.3, delay: 0.1, usingSpringWithDamping: 0.5, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+//            self.view.layoutIfNeeded()
+//        }, completion: nil)
+        
+        // 뷰 속성을 이용한 애니메이션
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.nameLabel.transform = CGAffineTransform.identity
+            self.nameLabel.alpha = 1
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 1, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.explainLabel.transform = CGAffineTransform.identity
+            self.explainLabel.alpha = 1
+        }, completion: nil)
+        
+        // 회전 애니메이션
+        UIView.transition(with: imageView, duration: 0.1, options: .transitionFlipFromLeft, animations: nil, completion: nil)
     }
     
     @IBAction func close(_ sender: Any) {
@@ -37,6 +80,9 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var explainLabel: UILabel!
+    
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var explainLabelY: NSLayoutConstraint!
     
     
     let viewModel = DetailViewModel()
